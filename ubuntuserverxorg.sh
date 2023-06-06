d9fdbac2188e570a9d24043dcd0a1a63c7065ebf
bob <108271839+minegamer72@users.noreply.github.com>
2023-06-06 01:49:15 -0400

    Add files via upload

---
+++ ubuntuserverxorg.sh
@@ -0,0 +1,9 @@
+# install first drivers
+sudo apt install xorg
+
+# for just building other programs like st
+sudo apt install build-essential
+
+# install other dependencies related to xorg
+sudo apt-get install libx11-dev libxft-dev libxinerama-dev libxrandr-dev libxft2 libx11-xcb-dev libxcb-util0-dev libxcb1-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-xinerama0-dev libxinerama1
+
