4cdcfb259aa73d6065aaa898075283b6da466833
bob <108271839+minegamer72@users.noreply.github.com>
2023-06-06 02:05:18 -0400

    Update ubuntuserverxorg.sh
    attempt fix

--- ubuntuserverxorg.sh
+++ ubuntuserverxorg.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 # install first drivers
-sudo apt install xorg
+apt install xorg
 
 # for just building other programs like st
-sudo apt install build-essential
+apt install build-essential
 
 # install other dependencies related to xorg
-sudo apt install libx11-dev libxft-dev libxinerama-dev libxrandr-dev libxft2 libx11-xcb-dev libxcb-util0-dev libxcb1-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-xinerama0-dev libxinerama1
+apt install libx11-dev libxft-dev libxinerama-dev libxrandr-dev libxft2 libx11-xcb-dev libxcb-util0-dev libxcb1-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-xinerama0-dev libxinerama1
 
